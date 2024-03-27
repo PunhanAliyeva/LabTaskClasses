d0b33f98ff22621a1e240ee1eb6b1feb0c1c4230
PunhanAliyeva <eliyevapunhan567@gmail.com>
2024-03-27 13:22:59 +0400

    Add project files.

---
+++ LabTaskClasses/Human.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTaskClasses
+{
+    internal class Human
+    {
+        public string Name;
+        public string Surname;
+        public byte Age;
+        public string City;
+
+        public Human(string name,string surname,byte age,string city)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            City = city;
+        }
+        public bool IsStudent()
+        {
+            if (Age > 18 && Age < 25)
+            {
+                return true;
+            }
+            return false;
+        }
+        public Human()
+        {
+            
+        }
+        public void CheckCity(string city1, string city2, string city3, string city4)
+        {
+            if (city1.Equals(city3))
+            {
+                Console.WriteLine("Eyni seherdedir..");
+            }
+            if (city2.Equals(city4))
+            {
+                Console.WriteLine("Eyni seherdedir..");
+            }
+            else
+            {
+                Console.WriteLine("Eyni seherde deyil..");
+            }
+        }
+
+    }
+}
---
+++ LabTaskClasses/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace LabTaskClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Human human1=new Human("Punhan","Aliyeva",21,"Siyazan");
+           Human human2=new Human("Aygun","Shahmuradova",22,"Xachmaz");
+           Human human3=new Human("Selma","Mammedli",17,"Siyazan");
+           Human human4=new Human("Ali","Aliyev",18,"Baku");
+
+           
+
+            //Console.WriteLine(human1.IsStudent());
+            //Console.WriteLine(human2.IsStudent());
+            //Console.WriteLine(human3.IsStudent());
+            //Console.WriteLine(human4.IsStudent());
+           
+
+            Human human = new();
+            human.CheckCity(human1.City,human2.City,human3.City,human4.City);
+
+        }
+    }
+}
